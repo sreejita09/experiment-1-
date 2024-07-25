c1e87face1dcc9db62b92bc3cfb43dd50c5c7a3e
Sreejita Bhardwaj <sreejitabhardwaj2005@gmail.com>
2024-07-25 09:18:54 +0530

    Add files via upload

---
+++ calculator.cpp
@@ -0,0 +1,29 @@
+//Sreejita Bhardwaj 
+//Prn: 23070123130 
+//experiment 1: calculator 
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a,b,sum=0, diff, mult, div ;
+    cout<<"enter a number a: ";
+    cin>>a;
+    cout<<"enter a number b: ";
+    cin>>b; 
+
+    sum=a+b;
+    cout<<"sum is: "<<sum<<endl; 
+
+    diff=a-b; 
+    cout<<"diff is: "<<diff<<endl;
+
+    mult=a*b;
+    cout<<"mult is: "<<mult<<endl;
+
+    div=a/b;
+    cout<<"division is: "<<div<<endl; 
+
+    return 0;
+}
